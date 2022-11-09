6c38d3d8cb61e67f9fec14501122d2d02204de72
FRA2721 <cimino.francesco36@gmail.com>
2022-11-09 10:24:50 +0100

    App.vue main setup

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<!-- dev: Francesco Cimino; -->
+<!-- proj: Vite DC Comics; -->
+
+
+
+<!-- script-section -->
+<script>
+</script>
+<!-- /script-section -->
+
+
+
+<!-- template-section -->
+<template>
+</template>
+<!-- /template-section -->
+
+
+
+<!-- style-section -->
+<style lang="scss">
+</style>
+<!-- /style-section -->
